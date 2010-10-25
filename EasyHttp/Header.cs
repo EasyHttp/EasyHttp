cd48b1d747b9194408582218b21876d45c83c891
hhariri <mail@hadiharir.com>
2010-10-26 00:09:25 +0200

    Removed old files

--- EasyHttp/Header.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EasyHttp
-{
-    public class Header
-    {
-        public string ContentType { get; set; }
-        public string Accept { get; set; }
-        public string StatusDescription { get; set; }
-    }
-}
