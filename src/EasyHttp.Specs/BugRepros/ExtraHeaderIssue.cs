3d42a68314652869b06ae81eda0612f6569d5600
hhariri <mail@hadihariri.com>
2011-11-13 20:07:30 +0100

    Added missing file

---
+++ src/EasyHttp.Specs/BugRepros/ExtraHeaderIssue.cs
@@ -0,0 +1,22 @@
+﻿using EasyHttp.Http;
+using Machine.Specifications;
+
+namespace EasyHttp.Specs.BugRepros
+{
+    public class when_adding_extra_header_to_request
+    {
+        Establish context = () =>
+            {
+                http = new HttpClient();
+
+            };
+
+        Because of = () =>
+            {
+                http.Request.AddExtraHeader("X-Header", "X-Value");
+            };
+
+        It should_add_it_to_the_request = () => { http.Request.RawHeaders.ContainsKey("X-Header"); };
+        static HttpClient http;
+    }
+}
