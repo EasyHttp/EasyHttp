a6c5308dc88dec3c802c480dcb6691f1763fceec
Johan Leino <leino@outlook.com>
2013-05-16 12:58:13 +0200

    Refactored ObjectToUrlxxx for DRY

---
+++ src/EasyHttp/Infrastructure/ObjectToUrl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+
+namespace EasyHttp.Infrastructure
+{
+    public abstract class ObjectToUrl
+    {
+        protected abstract string PathStartCharacter { get; }
+        protected abstract string PathSeparatorCharacter { get; }
+        protected abstract string BuildParam(PropertyValue propertyValue);
+
+        public string ParametersToUrl(object parameters)
+        {
+            var returnuri = "";
+            var properties = GetProperties((parameters));
+            if (parameters != null)
+            {
+                var paramsList = properties.Select(BuildParam).ToList();
+                if (paramsList.Count > 0)
+                {
+                    returnuri = String.Format("{0}{1}", PathStartCharacter, String.Join(PathSeparatorCharacter, paramsList));
+                }
+            }
+            return returnuri;
+        }
+
+        private static IEnumerable<PropertyValue> GetProperties(object parameters)
+        {
+            if (parameters == null) yield break;
+            if (parameters is ExpandoObject)
+            {
+                var dictionary = parameters as IDictionary<string, object>;
+                foreach (var property in dictionary)
+                {
+                    yield return new PropertyValue { Name = property.Key, Value = property.Value.ToString() };
+                }
+            }
+
+            var properties = TypeDescriptor.GetProperties(parameters);
+            foreach (PropertyDescriptor propertyDescriptor in properties)
+            {
+                var val = propertyDescriptor.GetValue(parameters);
+                if (val != null)
+                {
+                    yield return new PropertyValue { Name = propertyDescriptor.Name, Value = val.ToString() };
+                }
+            }
+        }
+
+        protected class PropertyValue
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
--- src/EasyHttp/Infrastructure/ObjectToUrlParameters.cs
+++ src/EasyHttp/Infrastructure/ObjectToUrlParameters.cs
@@ -1,53 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
 
 namespace EasyHttp.Infrastructure
 {
-    public class ObjectToUrlParameters
+    public class ObjectToUrlParameters : ObjectToUrl
     {
-        public string ParametersToUrl(object parameters)
+        protected override string PathStartCharacter
         {
-            var returnuri = "";
-            var properties = GetProperties((parameters));
-            if (parameters != null)
-            {
-                var paramsList = properties.Select(prop => String.Join("=", prop.Name, System.Web.HttpUtility.UrlEncode(prop.Value))).ToList();
-                if (paramsList.Count > 0)  returnuri = String.Format("?{0}", String.Join("&", paramsList));
-            }
-            return returnuri;
+            get { return "?"; }
         }
 
-        private IEnumerable<PropertyValue> GetProperties(object parameters)
+        protected override string PathSeparatorCharacter
         {
-            if (parameters == null) yield break;
-            if (parameters is ExpandoObject)
-            {
-                var dictionary = parameters as IDictionary<string, object>;
-                foreach (var property in dictionary)
-                {
-                    yield return new PropertyValue { Name = property.Key, Value = property.Value.ToString() };
-                }
-            }
-
-            var properties = TypeDescriptor.GetProperties(parameters);
-            foreach (PropertyDescriptor propertyDescriptor in properties)
-            {
-                var val = propertyDescriptor.GetValue(parameters);
-                if (val != null)
-                {
-                    yield return new PropertyValue { Name = propertyDescriptor.Name, Value = val.ToString() };
-                }
-            }
+            get { return "&"; }
         }
 
-        private class PropertyValue
+        protected override string BuildParam(PropertyValue propertyValue)
         {
-            public string Name { get; set; }
-            public string Value { get; set; }
+            return String.Join("=", propertyValue.Name, System.Web.HttpUtility.UrlEncode(propertyValue.Value));
         }
+
     }
 }
