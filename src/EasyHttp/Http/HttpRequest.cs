ada4cb520992fef552fadf438f5fe04c131f173b
jeremybeavon <jeremybeavon@users.noreply.github.com>
2016-04-07 07:43:44 +1200

    Add support for default credentials

--- src/EasyHttp/Http/HttpRequest.cs
+++ src/EasyHttp/Http/HttpRequest.cs
@@ -104,6 +104,7 @@ namespace EasyHttp.Http
         public IList<FileData> MultiPartFileData { get; set; }
         public int Timeout { get; set; }
         public Boolean ParametersAsSegments { get; set; }
+        public bool UseDefaultCredentials { get; set; }
 
         public bool ForceBasicAuth
         {
@@ -307,6 +308,10 @@ namespace EasyHttp.Http
                 string authInfo = _username + ":" + _password;
                 authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
                 httpWebRequest.Headers["Authorization"] = "Basic " + authInfo;
+            }
+            else if (UseDefaultCredentials)
+            {
+                httpWebRequest.UseDefaultCredentials = true;
             }
             else
             {
