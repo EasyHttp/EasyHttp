6b75725968bb0aaffe0172119369e1ab3c5a8b8e
David Alpert <dalpert@korewireless.com>
2016-12-23 15:34:48 -0600

    adds a pair of specifications to document current behavior
    - gzip and deflate headers are being added to the underlying
      HttpWebRequest regardless of what is explicitly set in
      HttpClient.Request.AcceptEncoding
    
    - the HttpRequest class uses an underlying System.Net.WebRequest
      which exposes an AutomaticDecompression Flags Enum.
    
    - the SetupHeader method explicitly sets the underlying HttpWebRequest
      to use GZip and Deflate decompression methods
      https://github.com/hhariri/EasyHttp/blob/master/src/EasyHttp/Http/HttpRequest.cs#L136
    
    - this in turn results in the underlying web request adding
      gzip, deflate to the Accept-Encoding http header which gets sent
      across the wire, regardless of whatever value the consumer sets
      for the HttpClient.Request.AcceptEncoding field.

---
+++ src/EasyHttp.Specs/Helpers/MSpecFlagAssertions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Machine.Specifications;
+using Machine.Specifications.Utility.Internal;
+
+namespace EasyHttp.Specs.Helpers
+{
+    public static class MSpecFlagAssertions
+    {
+        public static Enum ShouldHaveFlag(this Enum actual, Enum expected)
+        {
+            if (!actual.HasFlag(expected))
+                throw new SpecificationException(PrettyPrintingExtensions.FormatErrorMessage((object)actual, (object)expected));
+            return actual;
+        }
+
+        public static Enum ShouldNotHaveFlag(this Enum actual, Enum expected)
+        {
+            if (actual.HasFlag(expected))
+                throw new SpecificationException(string.Format("Should not have {0} set but does: {1}", (object)expected, ((object)actual)));
+            return actual;
+        }
+    }
+}
